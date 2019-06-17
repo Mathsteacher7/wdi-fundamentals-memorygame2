04cbc17db0e98718d14df4dddf4d95136a87a406
Daniel H Cohen <hofcohen@gmail.com>
2019-06-17 20:37:01 +0100

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,9 +1,19 @@
 console.log("Up and running");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardThree)
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+var cradTwo = cards[1];
+cardsInPlay.push(cradTwo);
+console.log("User flipped " + cradTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
+
